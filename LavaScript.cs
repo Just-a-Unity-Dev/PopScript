47a4779a88c64d4820d16155f49291d5ff2de871
blobHacker <67359748+blobHacker@users.noreply.github.com>
2020-08-10 16:15:15 +0800

    Add files via upload

---
+++ LavaScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public bool DestroyEverything = false;
+    public bool DisableSound = false;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (DestroyEverything == false) {
+            if (DisableSound == false) {
+                if (col.tag == "Target") {
+                    if (col.tag == "Target") {
+                        audioSource.Play();
+                        Destroy(col.gameObject);
+                    }
+                }
+            }
+            else {
+                if (col.tag == "Target") {
+                    Destroy(col.gameObject);
+                }
+            }
+
+        }
+        else {
+            if (DisableSound == false) {
+                audioSource.Play();
+                Destroy(col.gameObject);
+            }
+            else {
+                Destroy(col.gameObject);
+            }
+        }
+
+    }
+}
